8fc63235f1fd3b266e4992820b1f8ad9071b60fb
Artem Naumenko <entsupml@gmail.com>
2014-05-05 10:43:45 +0400

    initial 2

--- src/controllers/SiteController.php
+++ src/controllers/SiteController.php
@@ -27,7 +27,6 @@ class SiteController extends Controller
 
 	public function actionIndex()
 	{
-        vardumpd($_SERVER);
         $releaseRequestSearchModel=new ReleaseRequest('search');
         $releaseRequestSearchModel->unsetAttributes();  // clear any default values
         if(isset($_GET['ReleaseRequest']))
