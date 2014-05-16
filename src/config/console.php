88b0b7c9b1925890504aaca15c09de90a864f0f1
Artem Naumenko <entsupml@gmail.com>
2014-05-16 17:36:25 +0400

    [*] Config added

---
+++ src/config/console.php
@@ -0,0 +1,27 @@
+<?php
+
+// This is the configuration for yiic console application.
+// Any writable CConsoleApplication properties can be configured here.
+return array(
+	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+	'name'=>'My Console Application',
+
+	// preloading 'log' component
+	'preload'=>array('log'),
+
+	// application components
+	'components'=>array(
+        'authManager'=>array(
+            'class'=>'CPhpAuthManager',
+        ),
+		'log'=>array(
+			'class'=>'CLogRouter',
+			'routes'=>array(
+				array(
+					'class'=>'CFileLogRoute',
+					'levels'=>'error, warning',
+				),
+			),
+		),
+	),
+);
