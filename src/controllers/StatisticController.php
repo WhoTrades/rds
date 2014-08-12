84e15dd18bc235e368fcbd2915c27eb3cd183f6e
Artem Naumenko <entsupml@gmail.com>
2014-08-12 09:37:16 +0400

    [*] Added statistic controller to export building times to munin #WTA-7

---
+++ src/controllers/StatisticController.php
@@ -0,0 +1,24 @@
+<?php
+class StatisticController extends Controller
+{
+    public function actionGetLastBuildTime($projectName)
+    {
+        $project = Project::model()->findByAttributes(array('project_name' => $projectName));
+
+        if (!$project) {
+            throw new CHttpException(404, 'unknown project');
+        }
+
+        $c = new CDbCriteria();
+        $c->order = 'obj_id desc';
+        $c->condition = 'rr_project_obj_id='.(int)$project->obj_id;
+        /** @var $releaseRequest ReleaseRequest*/
+        $releaseRequest = ReleaseRequest::model()->find($c);
+
+        if ($releaseRequest && $releaseRequest->rr_built_time) {
+            echo strtotime($releaseRequest->rr_built_time) - strtotime($releaseRequest->obj_created);
+        } else {
+            echo "unknown";
+        }
+    }
+}
