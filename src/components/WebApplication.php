de8d653f5ad86c269e5cd005462560e02f847702
Artem Naumenko <entsupml@gmail.com>
2014-08-29 10:31:24 +0400

    [*] Added autocreating runtimePath folder if not exists

--- src/components/WebApplication.php
+++ src/components/WebApplication.php
@@ -11,4 +11,14 @@ class WebApplication extends CWebApplication
         }
         return parent::end();
     }
+
+    public function setRuntimePath($path)
+    {
+        //an: Создаем папку для временных файлов, если её ещё нету
+        if((false !== $runtimePath=realpath($path)) && !is_dir($runtimePath)) {
+            mkdir($runtimePath, 0777);
+        }
+
+        return parent::setRuntimePath($path);
+    }
 }
