2c09fd9b10dca1d70ec36a4360c66f6ad597f8cd
Artem Naumenko <entsupml@gmail.com>
2014-08-29 15:40:32 +0400

    [*] Required authorization for logs page viewing #WTA-27

--- src/controllers/LogController.php
+++ src/controllers/LogController.php
@@ -3,6 +3,26 @@
 class LogController extends Controller
 {
     public $layout = '//layouts/main';
+
+    public function filters()
+    {
+        return array(
+                'accessControl', // perform access control for CRUD operations
+        );
+    }
+
+    public function accessRules()
+    {
+        return array(
+            array('allow',  // allow all users to perform 'index' and 'view' actions
+                'users'=>array('@'),
+            ),
+            array('deny',  // deny all users
+                'users'=>array('*'),
+            ),
+        );
+    }
+
     /**
      * Displays a particular model.
      * @param integer $id the ID of the model to be displayed
