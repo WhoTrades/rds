17c5bc6af51bc426dae0759f4d89865607a5e664
Artem Naumenko <entsupml@gmail.com>
2014-09-10 14:19:57 +0400

    [*] Enlarged verbosity of cronjobs #WTA-26

--- misc/cron/ServiceRdsProdTL1.php
+++ misc/cron/ServiceRdsProdTL1.php
@@ -13,8 +13,8 @@ class ServiceRdsProdTL1
     public function getCronConfigRows()
     {
         $allCommands = [
-            new MultiCommandToCron(new MultiPeriodicCommand(\PgQ_EventProcessor_RdsJiraCreateVersion::getPgqConsumer('rds_jira_create_version', 'rds_jira_create_version_consumer', 'simple', 'DSN_DB4', 1, [], 1), 5), '* * * * *'),
-            new MultiCommandToCron(new MultiPeriodicCommand(\PgQ_EventProcessor_RdsJiraCommit::getPgqConsumer('rds_jira_commit', 'rds_jira_commit_consumer', 'simple', 'DSN_DB4', 1, [], 1), 5), '* * * * *'),
+            new MultiCommandToCron(new MultiPeriodicCommand(\PgQ_EventProcessor_RdsJiraCreateVersion::getPgqConsumer('rds_jira_create_version', 'rds_jira_create_version_consumer', 'simple', 'DSN_DB4', 1, [], 3), 5), '* * * * *'),
+            new MultiCommandToCron(new MultiPeriodicCommand(\PgQ_EventProcessor_RdsJiraCommit::getPgqConsumer('rds_jira_commit', 'rds_jira_commit_consumer', 'simple', 'DSN_DB4', 1, [], 3), 5), '* * * * *'),
         ];
 
         $allCommands = new MultiCronCommand($allCommands);
