c183b047f44a6d7d739029943bd46b24a24ee657
Artem Naumenko <entsupml@gmail.com>
2014-09-11 16:19:03 +0400

    [*] Added flagging and moving tickets due RDS actions #WTA-26

--- src/components/ExternalApplication.php
+++ src/components/ExternalApplication.php
@@ -7,4 +7,14 @@ class ExternalApplication extends CApplication
     {
         //ничего не делаем
     }
+
+    public function setRuntimePath($path)
+    {
+        //an: Создаем папку для временных файлов, если её ещё нету
+        if((false !== $runtimePath=realpath($path)) && !is_dir($runtimePath)) {
+            mkdir($runtimePath, 0777);
+        }
+
+        return parent::setRuntimePath($path);
+    }
 }
