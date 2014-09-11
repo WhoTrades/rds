b10c1de938393869054ca781ad564d7a57641568
Artem Naumenko <entsupml@gmail.com>
2014-09-11 16:25:49 +0400

    [*] Added flagging and moving tickets due RDS actions #WTA-26

--- src/components/WebApplication.php
+++ src/components/WebApplication.php
@@ -15,8 +15,8 @@ class WebApplication extends CWebApplication
     public function setRuntimePath($path)
     {
         //an: Создаем папку для временных файлов, если её ещё нету
-        if((false !== $runtimePath=realpath($path)) && !is_dir($runtimePath)) {
-            mkdir($runtimePath, 0777);
+        if(!is_dir($path)) {
+            mkdir($path, 0777);
         }
 
         return parent::setRuntimePath($path);
