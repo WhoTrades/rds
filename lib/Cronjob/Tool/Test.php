69f7a1f90db64014c00e9c2a503302c46564d577
Artem Naumenko <entsupml@gmail.com>
2014-10-16 12:30:34 +0400

    [*] Merged with release-65

--- lib/Cronjob/Tool/Test.php
+++ lib/Cronjob/Tool/Test.php
@@ -14,8 +14,7 @@ class Cronjob_Tool_Test extends RdsSystem\Cron\RabbitDaemon
     public function run(\Cronjob\ICronjob $cronJob)
     {
         $rdsSystem = new RdsSystem\Factory($this->debugLogger);
-        $workerName = \Config::getInstance()->workerName;
-        $model  = $rdsSystem->getMessagingRdsMsModel();
+        $model  = $this->getMessagingModel($cronJob);
         $migrations = ["Y2014_2/m140804_121502_rds_test #WTA-67"];
         $model->sendMigrations(new Message\ReleaseRequestMigrations('service-mailer', '62.00.042.42', $migrations, 'hard'));
     }
