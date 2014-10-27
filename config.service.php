ae89b0792b21673cea27fc3d128ca7df2f04aba9
Artem Naumenko <entsupml@gmail.com>
2014-10-27 19:30:46 +0400

    [-] Fixed typo with wrong configs

--- config.service.php
+++ config.service.php
@@ -4,8 +4,3 @@ $this->serviceRds['jira']['tagTickets'] = false;
 $this->serviceRds['jira']['transitionTickets'] = false;
 $this->serviceRds['jira']['checkTicketStatus'] = false;
 $this->environment = 'main';
-$this->installToPreprod = true;
-
-$this->hardMigration = [
-    'autoStartEnvironments' => ['preprod'],
-];
