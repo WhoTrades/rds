d3b41a663d64fc5620c0bc536499fb73f613c039
Artem Naumenko <entsupml@gmail.com>
2015-03-24 14:20:51 +0300

    [*] Fixed jira status names to english version #WTA-247

--- lib/Jira/Status.php
+++ lib/Jira/Status.php
@@ -3,18 +3,18 @@ namespace Jira;
 
 class Status
 {
-    const STATUS_READY_FOR_DEVELOPMENT = "Готово к разработке";
-    const STATUS_IN_PROGRESS = "В работе";
+    const STATUS_READY_FOR_DEVELOPMENT = "Ready for Develop";
+    const STATUS_IN_PROGRESS = "In Progress";
     const STATUS_IN_CONTINUOUS_INTEGRATION = "Continuous Integration";
-    const STATUS_CODE_REVIEW = "Проверка кода";
+    const STATUS_CODE_REVIEW = "Code Review";
     const STATUS_MERGE_TO_DEVELOP = "Merge to develop";
-    const STATUS_WAITING_FOR_TEST = "Ожидает тестирование";
-    const STATUS_TESTING = "Тестирование";
+    const STATUS_WAITING_FOR_TEST = "Ready for Test";
+    const STATUS_TESTING = "Testing";
     const STATUS_MERGE_TO_STAGING = "Merge to staging";
     const STATUS_READY_FOR_CHECK = "Ready for Check";
     const STATUS_CHECKING = "Checking";
     const STATUS_MERGE_TO_MASTER = "Merge to master";
-    const STATUS_READY_FOR_DEPLOYMENT = "Готово к выкладке";
-    const STATUS_READY_FOR_ACCEPTANCE = "Готово к приемке";
-    const STATUS_CLOSED = "Закрыт";
+    const STATUS_READY_FOR_DEPLOYMENT = "Ready for Deployment";
+    const STATUS_READY_FOR_ACCEPTANCE = "Ready for Acceptance";
+    const STATUS_CLOSED = "Closed";
 }
