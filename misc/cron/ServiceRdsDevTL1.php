52d20d58ca061d7e0dfc3e010e833072bc54374f
Artem Naumenko <entsupml@gmail.com>
2015-05-18 15:27:02 +0300

    [-] bugfix at cronjobs Master Tool
    (cherry picked from commit 5e7e6ca71d82e7443f64f205a4483245f8cf1df8)

--- misc/cron/ServiceRdsDevTL1.php
+++ misc/cron/ServiceRdsDevTL1.php
@@ -4,7 +4,7 @@ class ServiceRdsDevTL1 extends ServiceRdsTestTL1
    protected function getEnv()
     {
         return [
-            'CRONJOB_TOOLS=/home/an/dev/services/rds/misc/tools',
+            'CRONJOB_TOOLS=/home/dev/dev/services/rds/misc/tools',
         ];
     }
 }
