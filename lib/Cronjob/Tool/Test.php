3d3ddd990a814e4223192a46a71aa06ae6f75c79
Artem Naumenko <entsupml@gmail.com>
2015-08-20 12:40:11 +0300

    [+] Added setting pull request url to ticket #WTA-414

--- lib/Cronjob/Tool/Test.php
+++ lib/Cronjob/Tool/Test.php
@@ -17,88 +17,23 @@ class Cronjob_Tool_Test extends RdsSystem\Cron\RabbitDaemon
 
     public function run(\Cronjob\ICronjob $cronJob)
     {
-        /** @var $event PgQ_Event*/
-
-        $pullRequestTitle = 'Рецензия кода по задаче ...';
-        $pullRequestDescription = 'Рецензия создана автоматически, если она не нужна - просто закройте её';
+        $pullRequestTitle = 'test';
+        $pullRequestDescription = 'test';
         $pullRequestProject = 'WT';
         $pullRequestRepo = 'sparta';
-
-        $fromBranch = 'fg98fdgyhdfjk1';
+        $fromBranch = 'staging';
         $toBranch = 'master';
-        $jiraProject = 'WTA';
-
-        $httpSender = new \ServiceBase\HttpRequest\RequestSender($this->debugLogger);
-        $branches = json_decode($httpSender->getRequest("http://git.whotrades.net/branches.json", [], 30), true);
-        if (!$branches) {
-            throw new ApplicationException("Invalid json received from http://git.whotrades.net/branches.json");
-        }
-        if (empty($branches[$pullRequestRepo])) {
-            throw new ApplicationException("No $pullRequestRepo repository fount at http://git.whotrades.net/branches.json");
-        }
-
-        if (empty($branches[$pullRequestRepo][$fromBranch])) {
-            $this->debugLogger->message("Branch $fromBranch not found at http://git.whotrades.net/branches.json as repository $pullRequestRepo, so skip event");
-            return;
-        }
-
-        $jira = new JiraApi($this->debugLogger);
-
+        $assignee = ['anaumenko'];
         $stash = new \CompanyInfrastructure\StashApi($this->debugLogger);
+        $data = $stash->createPullRequest($pullRequestTitle, $pullRequestDescription, $pullRequestProject, $pullRequestRepo, $fromBranch, $toBranch, $assignee);
+        $url = "http://stash".$data['link']['url'];
 
-        $roles = $jira->getProjectInfo($jiraProject)['roles'];
+        $this->debugLogger->message($url);
 
-        if (empty(Yii::app()->params['stashPullRequestConfig'][$jiraProject])) {
-            $this->debugLogger->message("No roles for project $jiraProject assigned, skip event");
-            return;
-        }
+        return;
 
-        $assignee = [];
-        foreach (Yii::app()->params['stashPullRequestConfig'][$jiraProject]['roles'] as $role) {
-            $this->debugLogger->message($roles[$role]);
-            foreach ($jira->getProjectRolePeople($roles[$role]) as $people) {
-                foreach($people as $person) {
-                    $assignee[] = $person['name'];
-                }
-            }
-        }
 
-        try {
-            $stash->createPullRequest($pullRequestTitle, $pullRequestDescription, $pullRequestProject, $pullRequestRepo, $fromBranch, $toBranch, $assignee);
-        } catch (\ServiceBase\HttpRequest\Exception\ResponseCode $e) {
-            $this->debugLogger->error("Http code received: ".$e->getHttpCode());
-            $json = json_decode($e->getResponse(), true);
-            if (json_last_error()) {
-                $this->debugLogger->dump()->message('an', 'invalid_json_received_from_stash', false, [
-                    'eventData' => $event->getData(),
-                    'assignee' => $assignee,
-                    'httpCode' => $e->getHttpCode(),
-                    'httpResponse' => $e->getResponse(),
-                ])->critical()->save();
-
-                $event->retry(60);
-                return;
-            }
-            foreach ($json['errors'] as $error) {
-                switch ($error['exceptionName']) {
-                    case 'com.atlassian.stash.pull.DuplicatePullRequestException':
-                        //an: Такой pull request уже есть, игнорируем событие. @see https://developer.atlassian.com/static/javadoc/stash.old-perms-pre-feb4/1.3.0/api/reference/com/atlassian/stash/pull/DuplicatePullRequestException.html
-                        $url = $error['existingPullRequest']['links']['self'][0]['href'];
-                        $this->debugLogger->message("Pull request for branch $fromBranch already exists, skip it. URL: $url");
-                        return;
-                        break;
-                    default:
-                        $this->debugLogger->dump()->message('an', 'unknown_error_during_stash_request', false, [
-                            'eventData' => $event->getData(),
-                            'assignee' => $assignee,
-                            'httpCode' => $e->getHttpCode(),
-                            'response' => $json,
-                        ])->critical()->save();
-
-                        $event->retry(60);
-                        return;
-                }
-            }
-        }
+        $jira = new \JiraApi($this->debugLogger);
+        $jira->setCustomField('WTTES-47', 'customfield_15000', 'http://lenta.ru/');
     }
 }
