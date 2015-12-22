8dbe9d6987de01688fa621ebdafebcc6c9c29689
Artem Naumenko <entsupml@gmail.com>
2015-12-22 17:04:40 +0300

    [+] Added statistic of pgq event processing. Statistics send to graphite #WTA-337

---
+++ src/components/RdsEVentProcessorBase.php
@@ -0,0 +1,9 @@
+<?php
+abstract class RdsEventProcessorBase extends PgQ\EventProcessor\EventProcessorBase
+{
+    /** @return \GraphiteSystem\Graphite */
+    public function getEventProcessorGraphite()
+    {
+        return \Yii::app()->grephite->getGraphite();
+    }
+}
