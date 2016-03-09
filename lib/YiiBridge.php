3d8a87d23e46dec67c34f66143289bef51a867d7
Artem Naumenko <entsupml@gmail.com>
2016-03-09 16:29:15 +0300

    [*] Disabled some code style checks at migration and added codestyle error at YiiBridge #WTA-621

--- lib/YiiBridge.php
+++ lib/YiiBridge.php
@@ -6,7 +6,7 @@ class YiiBridge
         static $inited = false;
         if ($inited) return;
 
-        $config=dirname(__FILE__).'/../protected/config/main.php';
+        $config=dirname(__FILE__) . '/../protected/config/main.php';
         defined('YII_DEBUG') or define('YII_DEBUG',true);
         defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
