3cd1e28a01c24725a67b9d9276f6312178df2e7b
Artem Naumenko <entsupml@gmail.com>
2016-07-22 10:05:12 +0300

    [+] Cron jobs restore points moved to Redis from local files #WTA-202

---
+++ src/components/RdsRedis.php
@@ -0,0 +1,35 @@
+<?php
+use RedisSystem\IRedisClient;
+
+class RdsRedis extends CComponent
+{
+    public $host;
+    public $port;
+    public $timeout;
+    public $password;
+
+    private $redisClient;
+
+    /**
+     *
+     */
+    public function init()
+    {
+        $redisSystem = new RedisSystem\Factory([
+            'host' => $this->host,
+            'port' => $this->port,
+            'timeout' => $this->timeout,
+            'password' => $this->password,
+        ], Yii::app()->graphite->getGraphite());
+
+        $this->redisClient = $redisSystem->getRedisClient();
+    }
+
+    /**
+     * @return IRedisClient
+     */
+    public function getRedisClient()
+    {
+        return $this->redisClient;
+    }
+}
