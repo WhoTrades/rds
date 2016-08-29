fdffcba267bcece7b44c2725da5224bf54497698
auto_merge_bot <dev@whotrades.net>
2016-08-29 09:18:49 -0400

    fix typo

--- lib/RdsMigration.php
+++ lib/RdsMigration.php
@@ -7,25 +7,8 @@ class RdsMigration extends Cronjob\RequestHandler\Migration
         defined('YII_DEBUG') or define('YII_DEBUG', true);
         defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
 
-        include("yii/yii.php");
-
-        if (is_dir(__DIR__ . '/MigrationSystem')) {
-            $filename = __DIR__ . '/MigrationSystem/components/ConsoleApplication.php';
-        } else {
-            $filename = __DIR__ . '/../../../lib/MigrationSystem/components/ConsoleApplication.php';
-        }
-        
-        require_once($filename);
         $application = \Yii::createApplication('\MigrationSystem\components\ConsoleApplication', $config);
         $application->debugLogger = $debugLogger;
-
-        if (is_dir(__DIR__.'/MigrationSystem')) {
-            Yii::setPathOfAlias('MigrationSystem', __DIR__.'/MigrationSystem');
-        } else {
-            Yii::setPathOfAlias('MigrationSystem', __DIR__ . '/../../../lib/MigrationSystem');
-        }
-
-        Yii::import('MigrationSystem.components.*');
 
         parent::__construct($debugLogger);
     }
