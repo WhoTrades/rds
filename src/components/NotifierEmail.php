e927f6562a94e9f3a4143904aa8dd1245e1cb342
Artem Naumenko <entsupml@gmail.com>
2016-09-06 14:38:23 +0300

    [+] migration/comon/structure moved to migration/structure and RDS error of max open tasks per developer downgraded to warning #WTA-730

--- src/components/NotifierEmail.php
+++ src/components/NotifierEmail.php
@@ -35,6 +35,39 @@ class NotifierEmail extends CComponent
     /**
      * @param string $developerEmail
      * @param string $developerGroupEmail
+     * @param array  $ticketList
+     *
+     * @return bool
+     */
+    public function sendRdsDeveloperFeatureLimitExceeded($developerEmail, $developerGroupEmail, array $ticketList)
+    {
+        $ticketListString = implode(", ", $ticketList);
+
+        $text = <<<here
+Разработчик $developerEmail достиг лимита по количеству открытых задач.<br />
+Открытые задачи: $ticketListString<br /><br /
+
+Это письмо означает, что в процессе разработки существуют проблемы: задачи делаются, но не закрываются.<br />
+Нужно проанализировать на каком этапе происходит заминка и убрать её, либо перестать делать задачи, которые <br />
+останавливаются данной заминкой (например, если проблема в скорости тестирования - делаем задачи, не требующие тестирования)
+here;
+
+
+        $mail = new YiiMailer();
+        $mail->setBody($text);
+        $mail->setLayout('mail');
+        $mail->setFrom($this->mergeConflictEmail, 'releases');
+        $mail->setTo($developerEmail);
+        $mail->addCC($developerGroupEmail);
+        $mail->addCC("anaumenko@corp.finam.ru");
+        $mail->setSubject("[RDS] Достигнут лимит максимального количества открытых задач на разработчика");
+
+        return $mail->send();
+    }
+
+    /**
+     * @param string $developerEmail
+     * @param string $developerGroupEmail
      * @param string $ticket
      * @param string $targetBranch
      * @param string $text
