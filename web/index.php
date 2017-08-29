542bd511f2b320d49ae801cdbf6309758d92bba4
Artem Naumenko <entsupml@gmail.com>
2017-08-29 09:59:11 -0400

    [*] Fix path to rds/index.php #WTA-1281

--- web/index.php
+++ web/index.php
@@ -1,11 +1,11 @@
 <?php
-require_once(__DIR__ . "/../../vendor/autoload.php");
+require_once(__DIR__ . "/../vendor/autoload.php");
 
-$config = dirname(__FILE__) . '/../../protected/config/main.php';
+$config = dirname(__FILE__) . '/../protected/config/main.php';
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
 
-require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
+require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 $application = new \app\components\WebApplication(require($config));
 $application->run();
