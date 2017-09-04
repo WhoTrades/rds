fb5d7e7c2528e327b4eaee4a8e58e97a0e1d49e9
Artem Naumenko <entsupml@gmail.com>
2017-09-04 10:20:07 -0400

    [!] Added support of new version of phpunit #WTA-1292

---
+++ tests/unit/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers Email
+ */
+final class EmailTest extends TestCaseb/lib/DictionarySystem/phpunit-legacy.xml
+{
+    public function testCanBeCreatedFromValidEmailAddress()
+    {
+        $this->assertTrue(true);
+    }
+}
