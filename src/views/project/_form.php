2216704f41070db2e4be51c06a43e6d6c6d9fbae
Anton Gorlanov <ag@whotrades.org>
2018-01-26 13:34:15 +0300

    [*] Add addons to CodeMirrorWidget #WTA-1499

--- src/views/project/_form.php
+++ src/views/project/_form.php
@@ -6,6 +6,7 @@
  */
 
 use conquer\codemirror\CodemirrorWidget;
+use conquer\codemirror\CodemirrorAsset;
 use yii\bootstrap\Alert;
 use yii\bootstrap\Html;
 use yii\helpers\Url;
@@ -83,6 +84,11 @@ use kartik\select2\Select2;
                 'options' => [
                     'rows' => 15,
                     'style' => 'width: 100%',
+                ],
+                'assets' => [
+                    CodemirrorAsset::ADDON_DIALOG,
+                    CodemirrorAsset::ADDON_SEARCHCURSOR,
+                    CodemirrorAsset::ADDON_SEARCH,
                 ],
             ]);?>
         <?php } ?>
