2eac552d7a3b0f924ef080228e7312f92cdc67b7
Anton Gorlanov <antonxacc@gmail.com>
2018-05-31 10:31:47 +0300

    [*] Show target window icon #WTA-1641

--- src/views/site/_releaseRequestRow.php
+++ src/views/site/_releaseRequestRow.php
@@ -33,7 +33,7 @@ return array(
             if (Yii::$app->hasModule('Wtflow')) {
                 if ($releaseRequest->isInstalledStatus()) {
                     $result .= "<a href='" . yii\helpers\Url::to(['/Wtflow/jira/goto-jira-tickets-by-release-request', 'id' => $releaseRequest->obj_id]) .
-                        "' target='_blank'>Тикеты<img src='/images/open_new_window.png' alt='Open new window' style='margin-left:5px; margin-bottom:3px; width:13px;height:13px;'></a><br />";
+                        "' target='_blank' onclick=\"window.open(this.href,'_blank');return false;\">Тикеты<img src='/images/open_new_window.png' alt='Open new window' style='margin-left:5px; margin-bottom:3px; width:13px;height:13px;'></a><br />";
                 }
                 $result .= "<a href='/Wtflow/git/commits/?id=$releaseRequest->obj_id' onclick=\"popup('Комиты', this.href, {id: {$releaseRequest->obj_id}}); return false;\">Комиты</button>" .
                     "<img src='/images/open_popup_window.jpeg' alt='Open popup window' style='margin-left:5px; margin-bottom:3px; width:13px;height:13px;'>";
