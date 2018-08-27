dacd1cb4eedffa23f565d8a51b5315eb81b2160e
Anton Gorlanov <antonxacc@gmail.com>
2018-08-27 17:42:01 +0300

    [*] Move post migration handling to separate page #WTA-1732

--- src/migrations/m180821_121728_add_post_migrations_table.php
+++ src/migrations/m180821_121728_add_post_migrations_table.php
@@ -18,7 +18,7 @@ class m180821_121728_add_post_migrations_table extends Migration
                 "obj_modified" timestamp with time zone DEFAULT now() NOT NULL,
                 "obj_status_did" int2 DEFAULT 1 NOT NULL,
                 "pm_name" varchar COLLATE "default" NOT NULL,
-                "pm_status" int2 DEFAULT 5 NOT NULL, // ag: @see \whotrades\rds\models\PostMigration::STATUS_*
+                "pm_status" int2 DEFAULT 5 NOT NULL,
                 "pm_project_obj_id" int8 NOT NULL,
                 "pm_release_request_obj_id" int8 NOT NULL,
                 "pm_log" text
@@ -29,6 +29,7 @@ class m180821_121728_add_post_migrations_table extends Migration
 
         $this->execute('ALTER TABLE rds.post_migration ADD PRIMARY KEY (obj_id)');
         $this->execute('CREATE UNIQUE INDEX u_pm_name ON rds.post_migration (pm_name)');
+        $this->execute("COMMENT ON COLUMN rds.post_migration.pm_status IS 'ag: @see \\whotrades\\rds\\models\\PostMigration::STATUS_*'");
     }
 
     /**
