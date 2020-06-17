2ea981fc28ff398d4fe7a852af2511591dcc18f6
Anton Gorlanov <antonxacc@gmail.com>
2020-06-17 16:38:48 +0300

    [*] Remove unused notifications #WTA-1977

--- src/components/NotifierEmail.php
+++ src/components/NotifierEmail.php
@@ -78,56 +78,6 @@ class NotifierEmail extends \yii\base\BaseObject
     }
 
     /**
-     * @param string $developerEmail
-     * @param string $developerGroupEmail
-     * @param array  $ticketList
-     *
-     * @return bool
-     */
-    public function sendRdsDeveloperFeatureLimitExceeded($developerEmail, $developerGroupEmail, array $ticketList)
-    {
-        $ticketListString = implode(", ", $ticketList);
-
-        $text = <<<here
-Разработчик $developerEmail достиг лимита по количеству открытых задач.<br />
-Открытые задачи: $ticketListString<br /><br /
-
-Это письмо означает, что в процессе разработки существуют проблемы: задачи делаются, но не закрываются.<br />
-Нужно проанализировать на каком этапе происходит заминка и убрать её, либо перестать делать задачи, которые <br />
-останавливаются данной заминкой (например, если проблема в скорости тестирования - делаем задачи, не требующие тестирования)
-here;
-
-
-        $mail = $this->getMailer($text);
-        $mail->setFrom([$this->mergeConflictEmail => 'releases']);
-        $mail->setTo($developerEmail);
-        $mail->setCC($developerGroupEmail);
-        $mail->setSubject("[RDS] Достигнут лимит максимального количества открытых задач на разработчика");
-
-        return $mail->send();
-    }
-
-    /**
-     * @param string $developerEmail
-     * @param string $developerGroupEmail
-     * @param string $ticket
-     * @param string $targetBranch
-     * @param string $text
-     *
-     * @return bool
-     */
-    public function sendRdsConflictNotification($developerEmail, $developerGroupEmail, $ticket, $targetBranch, $text)
-    {
-        $mail = $this->getMailer($text);
-        $mail->setFrom([$this->mergeConflictEmail => 'releases']);
-        $mail->setTo($developerEmail);
-        $mail->setCC($developerGroupEmail);
-        $mail->setSubject("[RDS] Merge conflict at $ticket, branch=$targetBranch, developer=$developerEmail");
-
-        return $mail->send();
-    }
-
-    /**
      * @param string $user
      * @param string $projectName
      * @param string $comment
@@ -173,38 +123,6 @@ here;
         $mail->setFrom([$this->releaseReleasedEmail => 'releases']);
         $mail->setTo($this->releaseReleasedEmail);
         $mail->setSubject("[RDS] Состоялся релиз $projectName v. $newVersion");
-
-        return $mail->send();
-    }
-
-    /**
-     * @param string $receiver
-     * @param string $tagTo
-     * @param array $errors
-     * @return bool
-     */
-    public function sentNewSentryErrors($receiver, $tagTo, $errors)
-    {
-        $text = "<h1>Новые ошибки в релизе $tagTo:</h1>\n";
-        $summaryUsers = 0;
-        foreach ($errors as $error) {
-            $text .= "<a href='{$error['permalink']}'>{$error['title']}</a><br />\n";
-            $text .= "Всего ошибок: <b>{$error['count']}</b>, Пользователей затронуто: <b>{$error['userCount']}</b><br />\n";
-            $summaryUsers += $error['userCount'];
-            $text .= "<b>{$error['culprit']}</b><br />\n";
-            $text .= "<span style='color: gray'>{$error['metadata']['value']}</span><br /><br />\n\n";
-        }
-
-        $mail = $this->getMailer($text);
-        $mail->setFrom(['report@whotrades.org' => 'releases']);
-        $mail->setTo($receiver);
-        $mail->setSubject("[RDS] Новые ошибки после релиза $tagTo");
-
-        // an: Если ошибок много - добавляем в копию tml@whotrades.org
-        if ($summaryUsers > 10) {
-            $mail->setSubject("Ошибки в $tagTo, затронуто пользователей: $summaryUsers");
-            $mail->setCc('tml@whotrades.org');
-        }
 
         return $mail->send();
     }
